b7643cdf648379a20dbe9056cebb25cee4ee482f
Administrator <Administrator@scsbwin-493313.ITTPA.EBIZ.VERIZON.COM>
2015-10-30 05:48:44 -0700

    version update changes commited

---
+++ TGusageanalytics/src/main/java/com/vz/tg/model/HomeBean.java
@@ -0,0 +1,102 @@
+package com.vz.tg.model;
+
+import java.util.LinkedHashMap;
+import java.util.TreeMap;
+
+import org.json.JSONObject;
+
+public class HomeBean {
+	private long resultCount;
+	private long positiveCount;
+	private long negativeCount;
+	private long neutralCount;
+	private JSONObject ageGroup;
+	private JSONObject categoryData;
+	private int endRecord;
+	
+	
+	private TreeMap<String,Long> dataListRecords = new TreeMap<String,Long>();
+	private JSONObject dataUsageList;
+	
+	public JSONObject getDataUsageList() {
+		return dataUsageList;
+	}
+
+	public void setDataUsageList(JSONObject dataUsageList) {
+		this.dataUsageList = dataUsageList;
+	}
+
+	public TreeMap<String, Long> getDataListRecords() {
+		return dataListRecords;
+	}
+
+	public void setDataListRecords(TreeMap<String, Long> dataListRecords) {
+		this.dataListRecords = dataListRecords;
+	}
+
+	public int getEndRecord() {
+		return endRecord;
+	}
+
+	public void setEndRecord(int endRecord) {
+		this.endRecord = endRecord;
+	}
+
+	LinkedHashMap<String, String> tweetList = new LinkedHashMap<String, String>();
+
+	public LinkedHashMap<String, String> getTweetList() {
+		return tweetList;
+	}
+
+	public void setTweetList(LinkedHashMap<String, String> tweetList) {
+		this.tweetList = tweetList;
+	}
+
+	public JSONObject getCategoryData() {
+		return categoryData;
+	}
+
+	public void setCategoryData(JSONObject categoryData) {
+		this.categoryData = categoryData;
+	}
+
+	public JSONObject getAgeGroup() {
+		return ageGroup;
+	}
+
+	public void setAgeGroup(JSONObject ageGroup) {
+		this.ageGroup = ageGroup;
+	}
+
+	public long getPositiveCount() {
+		return positiveCount;
+	}
+
+	public void setPositiveCount(long positiveCount) {
+		this.positiveCount = positiveCount;
+	}
+
+	public long getNegativeCount() {
+		return negativeCount;
+	}
+
+	public void setNegativeCount(long negativeCount) {
+		this.negativeCount = negativeCount;
+	}
+
+	public long getNeutralCount() {
+		return neutralCount;
+	}
+
+	public void setNeutralCount(long neutralCount) {
+		this.neutralCount = neutralCount;
+	}
+
+	public long getResultCount() {
+		return resultCount;
+	}
+
+	public void setResultCount(long resultCount) {
+		this.resultCount = resultCount;
+	}
+}
--- TGusageanalytics/src/main/webapp/resources/js/app/common.js
+++ TGusageanalytics/src/main/webapp/resources/js/app/common.js
@@ -110,3 +110,43 @@ function requestHome(){
 function requestSearch(){
 	window.location.href = contextpath + "/search";
 }
+
+function generateDataPageBarChart(responseObj){
+	//alert(JSON.stringify(responseObj));
+	$('#morris-bar-chart-dataVol').empty();
+	var dataObj = responseObj.dataListByDate;
+	//alert(JSON.stringify(dataObj));
+    Morris.Bar({
+        element: 'morris-bar-chart-dataVol',
+        data: dataObj,
+        xkey: 'date',
+        ykeys: ['bytes'],
+        labels: ['Data Used (byes)'],
+        barRatio: 0.4,
+        xLabelAngle: 35,
+        hideHover: 'true',
+        resize: true
+    }).on('click', function(i, row){
+    	  console.log(i, row);
+    	  var ddDateObj = row;
+    	  alert(ddDateObj.date);
+    });
+}
+
+var getBytesWithUnit = function( bytes ){
+	//alert(bytes);
+	if( isNaN( bytes ) ){ return; }
+	var units = [ ' bytes', ' KB', ' MB', ' GB', ' TB', ' PB', ' EB', ' ZB', ' YB' ];
+	var amountOf2s = Math.floor( Math.log( +bytes )/Math.log(2) );
+	if( amountOf2s < 1 ){
+		amountOf2s = 0;
+	}
+	var i = Math.floor( amountOf2s / 10 );
+	bytes = +bytes / Math.pow( 2, 10*i );
+ 
+	// Rounds to 3 decimals places.
+        if( bytes.toString().length > bytes.toFixed(3).toString().length ){
+            bytes = bytes.toFixed(3);
+        }
+	return bytes + units[i];
+};
